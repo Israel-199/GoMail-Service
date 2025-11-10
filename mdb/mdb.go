e6972ef282fcfcb26eeb90fdcfe8beb993d84912
Israel-199 <israelassefa199@gmail.com>
2025-11-10 03:33:16 -0800

    adding the database operation

--- mdb/mdb.go
+++ mdb/mdb.go
@@ -1,8 +1,51 @@
 package mdb
 
+import (
+	"database/sql"
+	"log"
+	"time"
+	"github.com/mattn/go-sqlite3"
+)
+
 type EmailEntry struct {
-	Id          int
+	Id          int64
 	Email       string
-	ConfirmedAt int
-	OptOut      int
+	ConfirmedAt *time.Time
+	OptOut      bool
+}
+
+func TryCreate(db *sql.DB){
+	_,err:=db.Exec(`
+	CREATE TABLE emails (
+	id  INTEGER PRIMARY KEY,
+	email  TEXT UNIQUE,
+	confirmed_at  INTEGER,
+	opt_out   INTEGER
+	);
+	`)
+	if err != nil {
+		if sqlError, ok :=err.(sqlite3.Error); {
+			if sqlError.Code != 1{
+				log.Fatal(sqlError)
+			}
+		} else {
+			log.Fatal(err)
+		}
+	}
+}
+
+func emailEntryFromRow(row *sql.Rows)(*EmailEntry,error){
+	var id int64
+	var email string
+	var confirmedAt int64
+	var optOut bool
+
+	err:= row.Scan(&id,&email,&confiredAt,&optOut)
+
+	if err != nil {
+        log.Println(err)
+		return nil,err
+	}
+	t := time.Unix(confirmedAt,0)
+	return &EmailEntry{Id:id,Email: email,ConfirmedAt:&t,OptOut: optOut},nil
 }
