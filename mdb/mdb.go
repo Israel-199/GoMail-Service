df0fef3d66c3c98ebf4a41130ed06226812a4f7d
Israel-199 <israelassefa199@gmail.com>
2025-11-10 04:09:15 -0800

    adding the crud operation

--- mdb/mdb.go
+++ mdb/mdb.go
@@ -96,3 +96,50 @@ func UpdateEmail(db *sql.DB, email string)(*EmailEntry,error){
 
 	return nil
 }
+func DeleteEmail(db *sql.DB, email string) error {
+	_,err := db.Exec(`
+	UPDATE emails SET opt_out=true WHERE email=? 
+	`,email)
+	if err!=nil{
+		log.Println(err)
+		return err 
+	}
+
+	return nil
+}
+
+type GetEmailBatchQueryParams struct{
+	Page int
+	Count int
+}
+
+func GetEmailBatch(db *sql.DB,params GetEmailBatchQueryParams) ([]EmailEntry
+){
+	var empty []EmailEntry
+
+	row,err := db.Query(`
+	SELECT id, email, confirmed_at, opt_out
+	FROM emails
+	WHERE opt_out = false
+	ORDER BY id ASC
+	LIMIT ? OFFSET ?
+	`,params.Count,(params.Page-1)*params.Count)
+
+	if err!=nil{
+		log.Println(err)
+		return empty, err 
+	}
+defer row.Close()
+
+emails:= make([]EmailEntry,0,params.Count)	
+
+   for rows.Next(){
+		email,err := emailEntryFromRow(rows)
+		if err!=nil{
+		return nil, err 
+	}
+	emails = append(emails, *email)
+	}
+
+	return emails,nil
+}
