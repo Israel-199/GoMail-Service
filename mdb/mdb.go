761d9ab953b6da2c2026bd46b3a968c8ff88567c
Israel-199 <israelassefa199@gmail.com>
2025-11-10 03:54:33 -0800

    adding the crud operation

--- mdb/mdb.go
+++ mdb/mdb.go
@@ -49,3 +49,50 @@ func emailEntryFromRow(row *sql.Rows)(*EmailEntry,error){
 	t := time.Unix(confirmedAt,0)
 	return &EmailEntry{Id:id,Email: email,ConfirmedAt:&t,OptOut: optOut},nil
 }
+
+func CreateEmail(db *sql.DB, email string) error{
+	_,err := db.Exec(`INSERT INTO
+	emails(email,confirmed_at,opt_out)
+	VALUE(?,0,false)
+	`,email)
+	if err!=nil{
+		log.Println(err)
+		return err
+	}
+	return nil
+}
+
+func GetEmail(db *sql.DB, email string)(*EmailEntry,error){
+	row,err := db.Query(`
+	SELECT id, email, confirmed_at, opt_out
+	FROM emails
+	WHERE emails = ?
+	`,email)
+	if err!=nil{
+		log.Println(err)
+		return nil,err
+	}
+	defer row.Close()
+
+	for rows.Next(){
+		return emailEntryFromRow(rows)
+	}
+
+	return nil,nil
+}
+func UpdateEmail(db *sql.DB, email string)(*EmailEntry,error){
+	t := entry.ConfirmedAt.Unix()
+	_,err := db.Exec(`
+	emails(email, confirmed_at, opt_out)
+		VALUE(?,?,?)
+		ON CONFLICT(email) DO UPDATE SET
+		confirmed_at=?
+		opt_out=?
+	`,entry.Email,t,entry.OptOut,t,entry.OptOut)
+	if err!=nil{
+		log.Println(err)
+		return nil,err
+	}
+
+	return nil
+}
